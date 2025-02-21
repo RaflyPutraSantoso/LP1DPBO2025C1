5f1291467b1bb5178c9c6373ee461933d849c9c5
RaflyPutraSantoso <99879702+RaflyPutraSantoso@users.noreply.github.com>
2025-02-21 11:09:44 +0700

    add php

---
+++ Php/main.php
@@ -0,0 +1,12 @@
+<?php
+require('PetShop.php');
+//output
+echo"+----------------------------------------+<br>";
+echo"| Latihan Praktikum DPBO 1 2025 |<br>";
+echo"+----------------------------------------+<br>";
+
+
+$petshop = new PetShop();
+//tampilkan data yang telah di hardcore
+$petshop->tampil();
+?>
